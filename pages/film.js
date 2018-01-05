b6763a8d36a6dc12f78456164447b74d656859e3
iamkevingreen <kevin@thecouch.nyc>
2018-01-05 17:13:25 -0500

    drop in some postcss lib love, hook up some initial parts for mosaic

--- pages/film.js
+++ pages/film.js
@@ -4,6 +4,12 @@ import 'isomorphic-fetch'
 import contentfulAPI from 'api/contentful'
 
 export default class extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      activePart: 0
+    }
+  }
   static async getInitialProps ({query}) {
     const response = await contentfulAPI.getEntries({
       content_type: 'film',
@@ -14,22 +20,62 @@ export default class extends Component {
       film: response.items[0]
     }
   }
+  componentWillUpdate () {
+    setTimeout(() => {
+
+      const videoDom = document.getElementById(this.props.film.fields.parts[this.state.activePart].sys.id)
+      videoDom.play()
+    }, 200)
+  }
+  handleVideoParts (parts) {
+    const activePart = parts[this.state.activePart]
+    return (
+      <div>
+        <video id={activePart.sys.id} controls src={activePart.fields.videoUrl} />
+        <h5>{activePart.fields.name}</h5>
+      </div>
+    )
+  }
+  updateVideo (i) {
+    this.setState({
+      activePart: i,
+      playing: true
+    })
+  }
+  displayAllParts (parts) {
+    return parts.map((part, i) => {
+      return (
+        <div key={part.sys.id} onClick={() => this.updateVideo(i)} className={`part__link ${this.state.activePart === i ? 'active' : null}`}>{part.fields.name}</div>
+      )
+    })
+  }
   render () {
     const {
       title,
       releasedAt,
       company,
-      videoUrl
+      videoUrl,
+      parts
     } = this.props.film.fields
 
-    console.log('yo', this.props.film)
     return (
       <Layout>
         <h3>{title}</h3>
         {company && (<h5>{company.fields.name}</h5>)}
-        {videoUrl && (
-          <video src={videoUrl} controls />
-        )}
+        <div className='f jcb video__single'>
+          <div className='video__window'>
+            {videoUrl && (
+              <video src={videoUrl} controls />
+            )}
+            {parts && (
+              <div>{this.handleVideoParts(parts)}</div>
+            )}
+          </div>
+          <div className='ar video__parts'>
+            <h5>Parts</h5>
+            {this.displayAllParts(parts)}
+          </div>
+        </div>
       </Layout>
     )
   }
