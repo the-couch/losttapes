9029c8d5a57361c02af978cbdc956e3978b87d67
iamkevingreen <kevin@thecouch.nyc>
2018-01-05 12:15:43 -0500

    add in initial header information

--- components/cards/film.js
+++ components/cards/film.js
@@ -1,7 +1,11 @@
 import { Component } from 'react'
+import tinydate from 'tinydate'
+
+const time = tinydate('{DD}/{MM}/{YYYY}')
 
 export default (props) => (
   <div>
     <h3>{props.fields.title}</h3>
+    {props.fields.releasedAt && (<span className='small'>{time(new Date(props.fields.releasedAt))}</span>)}
   </div>
 )
--- components/sections/header.js
+++ components/sections/header.js
@@ -1,10 +1,19 @@
 const gray = '#223355'
-// import GlobalStyle from '../../styles/global'
+import Head from 'next/head'
 import stylesheet from '../../styles/main.css'
 import Link from 'next/link'
 
 export default () => (
   <div>
+    <Head>
+      <title>the lost tapes</title>
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      <meta name='description' content="a place for skateboarding to live after the vhs" />
+      <meta name='keywords' content='skateboarding, archive, collection, vhs' />
+      <meta name='og:url' content='https://thelosttapes.now.sh' />
+      <meta name='fragment' content='!' />
+      <meta http-equiv='Accept-CH' content='DPR, Width, Viewport-Width' />
+    </Head>
     <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
     <style jsx>{`
       .header {
