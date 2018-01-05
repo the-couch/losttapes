df58e9af3980971bec44e294c7ec59937f399972
iamkevingreen <kevin@thecouch.nyc>
2018-01-05 13:14:36 -0500

    custom routing hooked up for individual film pages

--- pages/film.js
+++ pages/film.js
@@ -1,12 +1,27 @@
 import { Component } from 'react'
 import Layout from 'components/layout'
+import 'isomorphic-fetch'
+import contentfulAPI from 'api/contentful'
 
 export default class extends Component {
+  static async getInitialProps ({query}) {
+    const response = await contentfulAPI.getEntries({
+      content_type: 'film',
+      'fields.slug': query.title,
+      include: 6
+    })
+    return {
+      film: response.items[0]
+    }
+  }
   render () {
-    console.log('sup film', this)
+    const {
+      title,
+      releasedAt
+    } = this.props.film.fields
     return (
       <Layout>
-        <h3>Taco</h3>
+        <h3>{title}</h3>
       </Layout>
     )
   }
