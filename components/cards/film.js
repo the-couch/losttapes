219c97400070129470ea7f0f395d634dcdcda9d9
iamkevingreen <kevin@thecouch.nyc>
2018-03-29 13:46:27 -0400

    add the fonts, change the site to black

--- components/cards/film.js
+++ components/cards/film.js
@@ -20,11 +20,16 @@ export default (props) => (
       }
       @media (min-width: 45em) {
         .film__card {
+          width: 49%;
+        }
+      }
+      @media (min-width: 68em) {
+        .film__card {
           width: 23%;
         }
       }
     `}</style>
-    <Link href={`/film?title=${props.fields.slug}`}><a className='abs fill fit'></a></Link>
+    <Link href={`/film?title=${props.fields.slug}`}><a className='abs z5 fill fit'></a></Link>
     <div className='color__block' style={{backgroundImage: 'url(' + props.fields.cover.fields.file.url + ')'}} />
     <h3 className='caps ls1'><Link href={`/film?title=${props.fields.slug}`}><a>{props.fields.title}</a></Link></h3>
     {props.fields.releasedAt && (<span className='small'>{time(new Date(props.fields.releasedAt))}</span>)}
