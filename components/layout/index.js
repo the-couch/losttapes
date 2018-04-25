7afaa92155ac3789e7abced110a5eb213103a964
iamkevingreen <thedudem4n@gmail.com>
2018-04-24 23:47:02 -0400

    set max width

--- components/layout/index.js
+++ components/layout/index.js
@@ -2,7 +2,7 @@ import Header from 'sections/header'
 import Footer from 'sections/footer'
 
 export default ({children, type}) => (
-  <div className='container'>
+  <div className='container container--l'>
     <Header type={type} />
     <div>{children}</div>
     <Footer />
